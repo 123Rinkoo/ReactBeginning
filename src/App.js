ad908c306d0bd2583038b4c1f09f8584b4a1ba4c
rinkoo <rinkookandpal1966@gmail.com>
2023-04-24 20:57:11 +0530

    repeating components n times

--- src/App.js
+++ src/App.js
@@ -3,19 +3,41 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
-  const locationofExpenditure=['Delhi','Lucknow','Ahmedabad','Bangalore','Surat','Guwahati','Hyderabad','Remote','Uttrakhand','Chandigarh'];
+  const locationofExpenditure=['Delhi','Lucknow','Ahmedabad','Banglore','Surat','Guwahati','Hyderabad','Remote','Uttrakhand','Chandigarh'];
+  const arr=[];
+  for(let i=0;i<100;i++)
+  {
+
+    arr.push(<ExpenseItem location={locationofExpenditure[Math.floor(Math.random()*10)]}></ExpenseItem>);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <h3>This is from React</h3>
-        
-        <ExpenseItem location={locationofExpenditure[0]}></ExpenseItem>
+        <h3>This is a tutorial of React</h3>
+        {/* <ExpenseItem location={locationofExpenditure[0]}></ExpenseItem>
         <ExpenseItem location={locationofExpenditure[1]}></ExpenseItem>
         <ExpenseItem location={locationofExpenditure[2]}></ExpenseItem>
-        <ExpenseItem location={locationofExpenditure[3]}></ExpenseItem>
+        <ExpenseItem location={locationofExpenditure[3]}></ExpenseItem> */}
+        {arr}
       </header>
     </div>
   );
 }
+
+// function App() {
+//   const locationofExpenditure=['Delhi','Lucknow','Ahmedabad','Bangalore','Surat','Guwahati','Hyderabad','Remote','Uttrakhand','Chandigarh'];
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h3>This is from React</h3>
+          
+//         <ExpenseItem location={locationofExpenditure[0]}></ExpenseItem>
+//         <ExpenseItem location={locationofExpenditure[1]}></ExpenseItem>
+//         <ExpenseItem location={locationofExpenditure[2]}></ExpenseItem>
+//         <ExpenseItem location={locationofExpenditure[3]}></ExpenseItem>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
