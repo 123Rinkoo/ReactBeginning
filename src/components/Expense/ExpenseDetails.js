b10b6b21d676d510c87220e2c2b87303b657485b
rinkoo <rinkookandpal1966@gmail.com>
2023-05-02 12:15:15 +0530

    applying useState

--- src/components/Expense/ExpenseDetails.js
+++ src/components/Expense/ExpenseDetails.js
@@ -1,14 +1,20 @@
+import React,{useState} from 'react';
+
 const ExpenseDetail=(props)=> {
+    var expense1='Food';
+    const [oldExpense, setExpense]=useState(expense1);
+    const updatingExpense=()=>{
+        setExpense('Momos');
+    }
     return (
         <div>
             <div>({props.location})</div>
-            <div>Food RS 10</div>
+            <div>{oldExpense} RS 10</div>
             <div>Petrol RS 100 </div>
             <div>Movies RS 200</div>
+        <button onClick={updatingExpense}>Update Expense</button>
         </div>
     );
-
-
 }
 
-export default ExpenseDetail;
+export default ExpenseDetail; 
